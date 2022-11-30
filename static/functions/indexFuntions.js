b3ab4070b1a0384048b244391f62c9e6fe6f30d9
matthew <matthewbeaulieu10@gmail.com>
2022-11-30 14:01:20 -0600

    added stylesheet and js file for index page

---
+++ static/functions/indexFuntions.js
@@ -0,0 +1,7 @@
+function testing() {
+    if (document.getElementById("urmom").style.color == "red") {
+        document.getElementById("urmom").style.color = "white";
+    } else {
+        document.getElementById("urmom").style.color = "red";
+    }
+}
